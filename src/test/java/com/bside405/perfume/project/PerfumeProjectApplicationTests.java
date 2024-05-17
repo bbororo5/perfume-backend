fd4211061f914cc173610908d1435da21fa3b256
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2024-05-18 03:04:41 +0900

    Initial commit

---
+++ src/test/java/com/bside405/perfume/project/PerfumeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside405.perfume.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
