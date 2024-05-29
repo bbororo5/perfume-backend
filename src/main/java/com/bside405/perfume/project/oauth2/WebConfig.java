b55fe3df569569110321f0744f308a47826f7205
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2024-05-29 10:36:20 +0900

    경로 수정

--- src/main/java/com/bside405/perfume/project/oauth2/WebConfig.java
+++ src/main/java/com/bside405/perfume/project/oauth2/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://perfume-bside.site")
+                        .allowedOrigins("https://frontend.perfume-bside.site")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
--- src/main/java/com/bside405/perfume/project/oauth2/security/SecurityConfig.java
+++ src/main/java/com/bside405/perfume/project/oauth2/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         http
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("https://perfume-bside.site"));
+                    config.setAllowedOrigins(Collections.singletonList("https://frontend.perfume-bside.site"));
                     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowCredentials(true);
                     config.setAllowedHeaders(Collections.singletonList("*"));
@@ -44,7 +44,7 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth2Login ->
                         oauth2Login
-                                .defaultSuccessUrl("https://perfume-bside.site/main", true)
+                                .defaultSuccessUrl("https://frontend.perfume-bside.site/main", true)
                                 .userInfoEndpoint(userInfo -> userInfo
                                         .userService(customOAuth2Service)
                                 )
@@ -54,7 +54,7 @@ public class SecurityConfig {
                                 .invalidateHttpSession(true)
                                 .deleteCookies("JSESSIONID")
                                 .logoutUrl("/api/logout")
-                                .logoutSuccessUrl("https://perfume-bside.site/")
+                                .logoutSuccessUrl("https://frontend.perfume-bside.site/")
                 )
 
                 .csrf(AbstractHttpConfigurer::disable)
