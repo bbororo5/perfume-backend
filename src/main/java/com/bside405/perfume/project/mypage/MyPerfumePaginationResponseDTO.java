46270a1f8cbdd7d3ca584146a1e4bba92af990b1
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2024-06-02 17:51:04 +0900

    dto에 getter, setter 추가

--- src/main/java/com/bside405/perfume/project/mypage/MyPerfumePaginationResponseDTO.java
+++ src/main/java/com/bside405/perfume/project/mypage/MyPerfumePaginationResponseDTO.java
@@ -1,13 +1,16 @@
 package com.bside405.perfume.project.mypage;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class MyPerfumePaginationResponseDTO {
     private List<MyPerfumeResponseDTO> content;
-    private int currentPage;
     private int totalPages;
     private long totalItems;
-
 }
